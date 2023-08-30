51e58ed4e434fd6172d0755b2727d067410c139a
Sona <hongson1234abb@gmail.com>
2023-08-30 16:44:59 +0700

    update faq.js

--- chlo'est/scripts/faq.js
+++ chlo'est/scripts/faq.js
@@ -2,12 +2,6 @@ const faqs = document.querySelectorAll(".faq-question");
 
 faqs.forEach((faq) => {
 	faq.addEventListener("click", () => {
-		faqs.forEach((question) => {
-			faq !== question ? question.classList.remove("faq-active") : undefined;
-		});
-
-		setTimeout(() => {
-			faq.classList.toggle("faq-active");
-		}, 300);
+		faq.classList.toggle("faq-active");
 	});
 });
