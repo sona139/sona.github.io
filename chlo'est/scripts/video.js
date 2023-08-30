8badf6b076ebc4e093f411d61b91af6dc9644bf6
Sona <hongson1234abb@gmail.com>
2023-08-30 23:51:14 +0700

    update responsive

--- chlo'est/scripts/video.js
+++ chlo'est/scripts/video.js
@@ -1,10 +1,10 @@
 const videoScreen = document.querySelector("#video");
 const video = document.querySelector(".video");
 const pauseBtn = document.querySelector(".video-btn");
-const videoBackground = document.querySelector(".video-background");
+// const videoBackground = document.querySelector(".video-background");
 
 videoScreen.addEventListener("click", () => {
 	video.paused ? video.play() : video.pause();
 	pauseBtn.classList.toggle("hide");
-	videoBackground.classList.toggle("hide");
+	// videoBackground.classList.toggle("hide");
 });
